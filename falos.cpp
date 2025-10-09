c309c8a2c5e7daf61e461cd2db1a4092070335f4
sendarggrg <100522016+sendarggrg@users.noreply.github.com>
2025-10-09 13:52:17 +0300

    Create falos.cpp

---
+++ falos.cpp
@@ -0,0 +1,122 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int a_1 = 10;
+    int a_2 = -5;
+    int a_3 = 1000;
+    int a_4 = 0;
+    int a_5 = 42;
+
+    cout << "a_1 = " << a_1 << endl;
+    cout << "a_2 = " << a_2 << endl;
+    cout << "a_4 = " << a_4 << endl;
+    cout << "a_5 = " << a_5 << endl;
+    cout << endl;
+
+    short s_1 = 1;
+    short s_2 = -100;
+    short s_3 = 30000;
+    short s_4 = 0;
+    short s_5 = 255;
+
+    cout << "s_1 = " << s_1 << endl;
+    cout << "s_2 = " << s_2 << endl;
+    cout << "s_3 = " << s_3  << endl;
+    cout << "s_4 = " << s_4  << endl;
+    cout << "s_5 = " << s_5  << endl;
+    cout << endl;
+
+    long l_1 = 100000L;
+    long l_2 = -50000L;
+    long l_3 = 123456789L;
+    long l_4 = 0L;
+    long l_5 = 999999999L;
+
+    cout << "l_2 = " << l_2 <<  endl;
+    cout << "l_3 = " << l_3 <<  endl;
+    cout << "l_4 = " << l_4 <<  endl;
+    cout << "l_5 = " << l_5 <<  endl;
+    cout << endl;
+
+    long long ll_1 = 10000000000LL;
+    long long ll_2 = -5000000000LL;
+    long long ll_3 = 123456789012LL;
+    long long ll_4 = 0LL;
+    long long ll_5 = 999999999999LL;
+
+    cout << "ll_1 = " << ll_1 << endl;
+    cout << "ll_2 = " << ll_2 << endl;
+    cout << "ll_3 = " << ll_3 << endl;
+    cout << "ll_4 = " << ll_4 << endl;
+    cout << "ll_5 = " << ll_5 << endl;
+    cout << endl;
+
+    float f_1 = 3.14f;
+    float f_2 = -2.5f;
+    float f_3 = 0.001f;
+    float f_4 = 100.0f;
+    float f_5 = -0.123f;
+
+    cout << "f_1 = " << f_1 << endl;
+    cout << "f_2 = " << f_2 << endl;
+    cout << "f_3 = " << f_3 << endl;
+    cout << "f_4 = " << f_4 << endl;
+    cout << "f_5 = " << f_5 << endl;
+    cout << endl;
+
+    double d_1 = 3.14159;
+    double d_2 = -2.71828;
+    double d_3 = 0.000001;
+    double d_4 = 12345.6789;
+    double d_5 = -9876.54321;
+
+    cout << "d_1 = " << d_1 << endl;
+    cout << "d_2 = " << d_2 << endl;
+    cout << "d_3 = " << d_3 << endl;
+    cout << "d_4 = " << d_4 << endl;
+    cout << "d_5 = " << d_5 << endl;
+    cout << endl;
+
+    char c_1 = 'A';
+    char c_2 = 'z';
+    char c_3 = '7';
+    char c_4 = '@';
+    char c_5 = ' ';
+
+    cout << "c_1 = " << c_1 << endl;
+    cout << "c_2 = " << c_2 << endl;
+    cout << "c_3 = " << c_3 << endl;
+    cout << "c_4 = " << c_4 << endl;
+    cout << "c_5 = " << c_5 << endl;
+    cout << endl;
+
+    bool b_1 = true;
+    bool b_2 = false;
+    bool b_3 = 1;
+    bool b_4 = 0;
+    bool b_5 = true;
+
+    cout << "b_1 = " << b_1 << endl;
+    cout << "b_2 = " << b_2 << endl;
+    cout << "b_3 = " << b_3 << endl;
+    cout << "b_4 = " << b_4 << endl;
+    cout << "b_5 = " << b_5 << endl;
+    cout << endl;
+
+    string str_1 = "Hello";
+    string str_2 = "World";
+    string str_3 = "C++";
+    string str_4 = "Programming";
+    string str_5 = "Test";
+
+    cout << "str_1 = " << str_1 << endl;
+    cout << "str_2 = " << str_2 << endl;
+    cout << "str_3 = " << str_3 << endl;
+    cout << "str_4 = " << str_4 << endl;
+    cout << "str_5 = " << str_5 << endl;
+
+    return 0;
+}
