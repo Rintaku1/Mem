d496965103c98e9b96895fcf929a4d0309c3eec9
Rintaku <100522016+sendarggrg@users.noreply.github.com>
2025-10-23 13:29:49 +0300

    Update Pr4.cpp

--- Pr4.cpp
+++ Pr4.cpp
@@ -1,18 +1,1 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-	int a;
-	int b = 1;
-	int c= 1;
-	int v = 1;
-	cin >> a;
-	while (b == c) {
-		cout << a << " * " << v << endl;
-		b = a * v;
-		cin >> c;
-		v++;
-	}
-	cout << "error";
-	return 0;
-}
+
